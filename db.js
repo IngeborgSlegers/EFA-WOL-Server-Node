3b46b7c3857942f082af1058945a43955acc2ca7
Ingeborg Slegers <ingeborgfreda@gmail.com>
2020-03-16 11:40:44 -0400

    updated database url in db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/workoutlog`, {
   dialect: 'postgres'
 })
 
